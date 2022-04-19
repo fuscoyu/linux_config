60b79c21b86339bd975ae5dd6a1c5db9e0b4ce94
fuscoyu <fuscoyu@gmail.com>
2022-04-19 22:32:08 +0800

    <feat>: add mac config

---
+++ mac/env.sh
@@ -0,0 +1,9 @@
+alias vi=nvim
+alias vim=nvim
+
+alias run-python="when-changed -r -v -1 . python3 "
+alias run-go="when-changed -r -v -1 . go run "
+
+export PATH=$PATH:/usr/local/go/bin:/usr/go/bin
+
+export GOPATH=/Users/luyu/go
