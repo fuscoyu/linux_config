70948e3978fc44282572e54b0c09810d892e44c2
fuscoyu <fuscoyu@gmail.com>
2022-04-19 21:35:24 +0800

    <feat>: add vim go plugin

--- wsl2/env.sh
+++ wsl2/env.sh
@@ -1,6 +1,18 @@
 alias vi=nvim
 alias vim=nvim
-export ALL_PROXY=socks5://192.168.1.197:10808
-# shmft 使用
-# exec sudo nsenter -t $(pidof systemd) -a su - $LOGNAME
+alias cdgo='cd ~/github/self/learning_go'
 
+alias cdk='cd ~/work/kolla/kolla-ansible'
+alias cdi='cd ~/work/kolla/installer'
+alias cdn='cd ~/work/openstack/nova'
+alias cdc='cd ~/work/openstack/cyborg'
+alias cdb='cd ~/work/build/'
+
+alias run-python="when-changed -r -v -1 . python3 "
+alias run-go="when-changed -r -v -1 . go run "
+
+export http_proxy=http://192.168.1.197:10809
+export https_proxy=http://192.168.1.197:10809
+
+export PATH=$PATH:/usr/local/go/bin:/usr/go/bin
+export GOPATH=/home/fuscoyu/go
