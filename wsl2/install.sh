3f790859d308d140ef2ee4b3bcc6bf97b65cba1f
fuscoyu <fuscoyu@gmail.com>
2022-05-25 09:54:35 +0800

    <feat>(wsl2): add install.sh script

---
+++ wsl2/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex 
+# ----------------------------------------------------------------------  
+# Copy configuration files 
+# ----------------------------------------------------------------------  
+cp ./config/zshrc $HOME/.zshrc
+cp ./config/p10k.zsh $HOME/.p10k.zsh
+
+mkdir -p $HOME/.config/nvim/
+cp ./config/vimrc $HOME/.vimrc
+cp ./config/init.vim $HOME/.config/nvim/.init.vim
+cp ./config/env.sh $HOME/.env.sh
+
+cp ./config/git/gitconfig $HOME/.gitconfig
+cp ./config/git/gitmessage $HOME/.gitmessage
+cp ./config/git/gitignore_global $HOME/.gitignore_global
+
