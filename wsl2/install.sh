2d6f64d0b125c22238f5ae73c10e6e7d41a23c3a
fuscoyu <fuscoyu@gmail.com>
2022-05-25 10:52:59 +0800

    <fix>(wsl2): fix file fromat error ''

--- wsl2/install.sh
+++ wsl2/install.sh
@@ -5,12 +5,14 @@ set -ex
 # ----------------------------------------------------------------------
 # Copy configuration files
 # ----------------------------------------------------------------------
+cp ./config/tmux.conf $HOME/.tmux.conf
+
 cp ./config/zshrc $HOME/.zshrc
 cp ./config/p10k.zsh $HOME/.p10k.zsh
 
 mkdir -p $HOME/.config/nvim/
 cp ./config/nvim/vimrc $HOME/.vimrc
-cp ./config/nvim/init.vim $HOME/.config/nvim/.init.vim
+cp ./config/nvim/init.vim $HOME/.config/nvim/init.vim
 
 cp ./config/git/gitconfig $HOME/.gitconfig
 cp ./config/git/gitmessage $HOME/.gitmessage
