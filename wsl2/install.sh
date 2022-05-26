292361be819c04fead5abdef2ef33856434f179f
fuscoyu <fuscoyu@gmail.com>
2022-05-26 16:35:34 +0800

    <refactor>(wsl2): mv neovim config to nvimdotsfile git repositories

--- wsl2/install.sh
+++ wsl2/install.sh
@@ -10,10 +10,6 @@ cp ./config/tmux.conf $HOME/.tmux.conf
 cp ./config/zshrc $HOME/.zshrc
 cp ./config/p10k.zsh $HOME/.p10k.zsh
 
-mkdir -p $HOME/.config/nvim/
-cp ./config/nvim/vimrc $HOME/.vimrc
-cp ./config/nvim/init.vim $HOME/.config/nvim/init.vim
-
 cp ./config/git/gitconfig $HOME/.gitconfig
 cp ./config/git/gitmessage $HOME/.gitmessage
 cp ./config/git/gitignore_global $HOME/.gitignore_global
